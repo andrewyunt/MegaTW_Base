49fac941885e8c93e2bde0963f09cd12990124d4
Andrew Yunt <andrew.yunt@gmail.com>
2016-11-12 17:39:41 -0800

    Change custom entity spawn code

--- src/com/andrewyunt/megatw_base/utilities/Utils.java
+++ src/com/andrewyunt/megatw_base/utilities/Utils.java
@@ -1,6 +1,5 @@
 package com.andrewyunt.megatw_base.utilities;
 
-import net.minecraft.server.v1_7_R4.EntityInsentient;
 import net.minecraft.server.v1_7_R4.EntityTypes;
 import net.minecraft.server.v1_7_R4.NBTTagCompound;
 import net.minecraft.server.v1_7_R4.NBTTagList;
@@ -17,8 +16,6 @@ import org.bukkit.util.Vector;
 import org.bukkit.craftbukkit.v1_7_R4.inventory.CraftItemStack;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -27,8 +24,8 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
- * The general utilities class for methods without a category / methods yet
- * to be categorized into another class.
+ * The general utilities class for methods without a category / methods yet to
+ * be categorized into another class.
  * 
  * @author Andrew Yunt
  * @author md_5
@@ -37,58 +34,81 @@ import java.util.stream.Collectors;
  * @author XlordalX
  */
 public class Utils {
+
+	protected static Field mapStringToClassField, mapClassToStringField, mapClassToIdField,
+		mapStringToIdField, mapIdToClassField;
+
+	static {
+		try {
+			mapStringToClassField = EntityTypes.class.getDeclaredField("c");
+			mapClassToStringField = EntityTypes.class.getDeclaredField("d");
+			mapIdToClassField = EntityTypes.class.getDeclaredField("e");
+			mapClassToIdField = EntityTypes.class.getDeclaredField("f");
+			mapStringToIdField = EntityTypes.class.getDeclaredField("g");
+
+			mapStringToClassField.setAccessible(true);
+			mapClassToStringField.setAccessible(true);
+			mapIdToClassField.setAccessible(true);
+			mapClassToIdField.setAccessible(true);
+			mapStringToIdField.setAccessible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	/**
-	 * @author Svenna
-	 * 
-	 * @see {@link https://www.spigotmc.org/threads/tutorial-register-and-use-nms-entities-1-8.77607/}
+	 * @author Jogy34
+	 *  
+	 * @see {@link https://bukkit.org/threads/tutorial-1-7-creating-a-custom-entity.212849/}
 	 */
-	public static void registerEntity(String name, int id, Class<? extends EntityInsentient> nmsClass,
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void addCustomEntity(Class entityClass, String name, int id) {
 		
-		Class<? extends EntityInsentient> customClass) {
-		
-        try {
-            List<Map<?, ?>> dataMap = new ArrayList<Map<?, ?>>();
-            for (Field f : EntityTypes.class.getDeclaredFields())
-                if (f.getType().getSimpleName().equals(Map.class.getSimpleName())){
-                    f.setAccessible(true);
-                    dataMap.add((Map<?, ?>) f.get(null));
-                }
-            
-            if (dataMap.get(2).containsKey(id)){
-                dataMap.get(0).remove(name);
-                dataMap.get(2).remove(id);
-            }
-     
-            Method method = EntityTypes.class.getDeclaredMethod("a", Class.class, String.class, int.class);
-            method.setAccessible(true);
-            method.invoke(null, customClass, name, id);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		if (mapStringToClassField == null || mapStringToIdField == null 
+				|| mapClassToStringField == null || mapClassToIdField == null)
+			return;
+		else
+			try {
+				Map mapStringToClass = (Map) mapStringToClassField.get(null);
+				Map mapStringToId = (Map) mapStringToIdField.get(null);
+				Map mapClasstoString = (Map) mapClassToStringField.get(null);
+				Map mapClassToId = (Map) mapClassToIdField.get(null);
+				
+				mapStringToClass.put(name, entityClass);
+				mapStringToId.put(name, Integer.valueOf(id));
+				mapClasstoString.put(entityClass, name);
+				mapClassToId.put(entityClass, Integer.valueOf(id));
+				
+				mapStringToClassField.set(null, mapStringToClass);
+				mapStringToIdField.set(null, mapStringToId);
+				mapClassToStringField.set(null, mapClasstoString);
+				mapClassToIdField.set(null, mapClassToId);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
 	
-    /**
-     * @author XlordalX
-     */
-    public static Object getPrivateField(String fieldName, @SuppressWarnings("rawtypes") Class clazz, Object object) {
-    	
-    	Field field;
-    	Object o = null;
-    	
-    	try {
-    		field = clazz.getDeclaredField(fieldName);
-    		
-    		field.setAccessible(true);
-    		
-    		o = field.get(object);
-    	} catch (NoSuchFieldException | IllegalAccessException e) {
-    		e.printStackTrace();
-    	}
-    	
-    	return o;
-    }
-    
+	/**
+	 * @author XlordalX
+	 */
+	public static Object getPrivateField(String fieldName, @SuppressWarnings("rawtypes") Class clazz, Object object) {
+
+		Field field;
+		Object o = null;
+
+		try {
+			field = clazz.getDeclaredField(fieldName);
+
+			field.setAccessible(true);
+
+			o = field.get(object);
+		} catch (NoSuchFieldException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
+		return o;
+	}
+
 	public static Location deserializeLocation(ConfigurationSection section) {
 
 		return new Location(Bukkit.getWorld(section.getString("w")), section.getDouble("x"), section.getDouble("y"),
@@ -106,184 +126,184 @@ public class Utils {
 
 		return map;
 	}
-	
+
 	public static List<String> colorizeList(List<String> list, ChatColor color) {
 
 		return list.stream().map(line -> color + line).collect(Collectors.toList());
 	}
 
-    
-    public static List<org.bukkit.entity.Entity> getNearbyEntities(Location l, int distance){
+	public static List<org.bukkit.entity.Entity> getNearbyEntities(Location l, int distance) {
 
-		return l.getWorld().getEntities().stream().filter(e -> l.distanceSquared(e.getLocation()) <= distance * distance).collect(Collectors.toList());
-    }
-    
-    /**
-     * Rotates the given vector around the y axis.
-     * This modifies the given vector.
-     *
-     * @author blablubbabc
-     * @param vector
-     *            the vector to rotate
-     * @param angleD
-     *            the angle of the rotation in degrees
-     * @return the given vector rotated
-     */
-    public static Vector rotateYAxis(Vector vector, double angleD) {
-    	
-    	vector = vector.clone();
-    	
-        // Validate.notNull(vector);
-        if (angleD == 0.0D)
-        	return vector;
- 
-        double angleR = Math.toRadians(angleD);
-        double x = vector.getX();
-        double z = vector.getZ();
-        double cos = Math.cos(angleR);
-        double sin = Math.sin(angleR);
- 
-        vector.setX(x * cos + z * (-sin));
-        vector.setZ(x * sin + z * cos);
- 
-        return vector;
-    }
-    
-    public static Inventory fromChest(Inventory inv) {
-    	
-    	Inventory newInv = Bukkit.createInventory(null, 36);
-    	
-    	for (int i = 0; i <= 26; i++) {
-    		ItemStack is = inv.getItem(i);
-    		
-    		if (is == null)
-    			continue;
-    		
-    		newInv.setItem(i + 9, is.clone());
-    	}
-    	
-    	for (int i = 27; i <= 35; i++) {
-    		ItemStack is = inv.getItem(i);
-    		
-    		if (is == null)
-    			continue;
-    		
-    		newInv.setItem(i - 27, is.clone());
-    	}
-    	
+		return l.getWorld().getEntities().stream()
+				.filter(e -> l.distanceSquared(e.getLocation()) <= distance * distance).collect(Collectors.toList());
+	}
+
+	/**
+	 * Rotates the given vector around the y axis. This modifies the given
+	 * vector.
+	 *
+	 * @author blablubbabc
+	 * @param vector
+	 *            the vector to rotate
+	 * @param angleD
+	 *            the angle of the rotation in degrees
+	 * @return the given vector rotated
+	 */
+	public static Vector rotateYAxis(Vector vector, double angleD) {
+
+		vector = vector.clone();
+
+		// Validate.notNull(vector);
+		if (angleD == 0.0D)
+			return vector;
+
+		double angleR = Math.toRadians(angleD);
+		double x = vector.getX();
+		double z = vector.getZ();
+		double cos = Math.cos(angleR);
+		double sin = Math.sin(angleR);
+
+		vector.setX(x * cos + z * (-sin));
+		vector.setZ(x * sin + z * cos);
+
+		return vector;
+	}
+
+	public static Inventory fromChest(Inventory inv) {
+
+		Inventory newInv = Bukkit.createInventory(null, 36);
+
+		for (int i = 0; i <= 26; i++) {
+			ItemStack is = inv.getItem(i);
+
+			if (is == null)
+				continue;
+
+			newInv.setItem(i + 9, is.clone());
+		}
+
+		for (int i = 27; i <= 35; i++) {
+			ItemStack is = inv.getItem(i);
+
+			if (is == null)
+				continue;
+
+			newInv.setItem(i - 27, is.clone());
+		}
+
 		return newInv;
-    }
-    
-    public static Inventory toChest(Inventory inv) {
-    	
-    	Inventory newInv = Bukkit.createInventory(null, 36);
-    	
-    	for (int i = 0; i <= 8; i++) {
-    		ItemStack is = inv.getItem(i);
-    		
-    		if (is == null)
-    			continue;
-    		
-    		newInv.setItem(i + 27, is.clone());
-    	}
-    	
-    	for (int i = 9; i <= 34; i++) {
-    		ItemStack is = inv.getItem(i);
-    		
-    		if (is == null)
-    			continue;
-    		
-    		newInv.setItem(i - 9, is.clone());
-    	}
-    	
+	}
+
+	public static Inventory toChest(Inventory inv) {
+
+		Inventory newInv = Bukkit.createInventory(null, 36);
+
+		for (int i = 0; i <= 8; i++) {
+			ItemStack is = inv.getItem(i);
+
+			if (is == null)
+				continue;
+
+			newInv.setItem(i + 27, is.clone());
+		}
+
+		for (int i = 9; i <= 34; i++) {
+			ItemStack is = inv.getItem(i);
+
+			if (is == null)
+				continue;
+
+			newInv.setItem(i - 9, is.clone());
+		}
+
 		return newInv;
-    }
-    
-    public static int getHighestEntry(ConfigurationSection section) {
-    	
-    	int highest = 0;
-    	
-    	if (section == null)
-    		return 1;
-    	
-    	Set<String> keys = section.getKeys(false);
-    	
-    	if (keys.size() == 0)
-    		return 0;
-    	
-    	for (String key : section.getKeys(false)) {
-    		int num = Integer.valueOf(key);
-    		
-    		if (highest < num)
-    			highest = num;
-    	}
-    	
-    	return highest;
-    }
-    
-    public static String getNumberSuffix(int num) {
-    	
-    	num = num % 100;
-    	
-        if (num >= 11 && num <= 13)
-            return "th";
-        
-        switch (num % 10) {
-        case 1:
-            return "st";
-        case 2:
-            return "nd";
-        case 3:
-            return "rd";
-        default:
-            return "th";
-        }
-    }
-    
-    public static ItemStack addGlow(ItemStack item){ 
-    	
-    	net.minecraft.server.v1_7_R4.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
-    	NBTTagCompound tag = null;
-    	
-    	if (!nmsStack.hasTag()) {
-    		tag = new NBTTagCompound();
-    		nmsStack.setTag(tag);
-    	}
-    	
-    	if (tag == null)
-    		tag = nmsStack.getTag();
-    	
-    	NBTTagList ench = new NBTTagList();
-    	tag.set("ench", ench);
-    	nmsStack.setTag(tag);
-    	
-    	return CraftItemStack.asCraftMirror(nmsStack);
-    }
-    
-    /**
-     * @author Quackster 
-     */
-    public static Set<Block> getBlocksBetweenTwoPoints(Location loc1, Location loc2) {
-    	
-        Set<Block> blocks = new HashSet<Block>();
- 
-        int topBlockX = (loc1.getBlockX() < loc2.getBlockX() ? loc2.getBlockX() : loc1.getBlockX());
-        int bottomBlockX = (loc1.getBlockX() > loc2.getBlockX() ? loc2.getBlockX() : loc1.getBlockX());
- 
-        int topBlockY = (loc1.getBlockY() < loc2.getBlockY() ? loc2.getBlockY() : loc1.getBlockY());
-        int bottomBlockY = (loc1.getBlockY() > loc2.getBlockY() ? loc2.getBlockY() : loc1.getBlockY());
- 
-        int topBlockZ = (loc1.getBlockZ() < loc2.getBlockZ() ? loc2.getBlockZ() : loc1.getBlockZ());
-        int bottomBlockZ = (loc1.getBlockZ() > loc2.getBlockZ() ? loc2.getBlockZ() : loc1.getBlockZ());
-        
-        for(int x = bottomBlockX; x <= topBlockX; x++)
-            for(int z = bottomBlockZ; z <= topBlockZ; z++)
-                for(int y = bottomBlockY; y <= topBlockY; y++) {
-                    Block block = loc1.getWorld().getBlockAt(x, y, z);
-                   
-                    blocks.add(block);
-                }
-        
-        return blocks;
-    }
+	}
+
+	public static int getHighestEntry(ConfigurationSection section) {
+
+		int highest = 0;
+
+		if (section == null)
+			return 1;
+
+		Set<String> keys = section.getKeys(false);
+
+		if (keys.size() == 0)
+			return 0;
+
+		for (String key : section.getKeys(false)) {
+			int num = Integer.valueOf(key);
+
+			if (highest < num)
+				highest = num;
+		}
+
+		return highest;
+	}
+
+	public static String getNumberSuffix(int num) {
+
+		num = num % 100;
+
+		if (num >= 11 && num <= 13)
+			return "th";
+
+		switch (num % 10) {
+		case 1:
+			return "st";
+		case 2:
+			return "nd";
+		case 3:
+			return "rd";
+		default:
+			return "th";
+		}
+	}
+
+	public static ItemStack addGlow(ItemStack item) {
+
+		net.minecraft.server.v1_7_R4.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
+		NBTTagCompound tag = null;
+
+		if (!nmsStack.hasTag()) {
+			tag = new NBTTagCompound();
+			nmsStack.setTag(tag);
+		}
+
+		if (tag == null)
+			tag = nmsStack.getTag();
+
+		NBTTagList ench = new NBTTagList();
+		tag.set("ench", ench);
+		nmsStack.setTag(tag);
+
+		return CraftItemStack.asCraftMirror(nmsStack);
+	}
+
+	/**
+	 * @author Quackster
+	 */
+	public static Set<Block> getBlocksBetweenTwoPoints(Location loc1, Location loc2) {
+
+		Set<Block> blocks = new HashSet<Block>();
+
+		int topBlockX = (loc1.getBlockX() < loc2.getBlockX() ? loc2.getBlockX() : loc1.getBlockX());
+		int bottomBlockX = (loc1.getBlockX() > loc2.getBlockX() ? loc2.getBlockX() : loc1.getBlockX());
+
+		int topBlockY = (loc1.getBlockY() < loc2.getBlockY() ? loc2.getBlockY() : loc1.getBlockY());
+		int bottomBlockY = (loc1.getBlockY() > loc2.getBlockY() ? loc2.getBlockY() : loc1.getBlockY());
+
+		int topBlockZ = (loc1.getBlockZ() < loc2.getBlockZ() ? loc2.getBlockZ() : loc1.getBlockZ());
+		int bottomBlockZ = (loc1.getBlockZ() > loc2.getBlockZ() ? loc2.getBlockZ() : loc1.getBlockZ());
+
+		for (int x = bottomBlockX; x <= topBlockX; x++)
+			for (int z = bottomBlockZ; z <= topBlockZ; z++)
+				for (int y = bottomBlockY; y <= topBlockY; y++) {
+					Block block = loc1.getWorld().getBlockAt(x, y, z);
+
+					blocks.add(block);
+				}
+
+		return blocks;
+	}
 }
